e309ddc172fb69358b2d4c848eb8a2a4c0741142
RevoluPowered <gordon.a.macpherson@gmail.com>
2017-01-07 14:24:19 +0000

    Added bullets and made char controllable.

---
+++ Assets/Bullet.cs
@@ -0,0 +1,8 @@
+using UnityEngine;
+
+public class Bullet : MonoBehaviour {
+	void Start()
+    {
+        Destroy(this.gameObject,0.5f);
+    }    
+}
--- Assets/Controller.cs
+++ Assets/Controller.cs
@@ -7,6 +7,7 @@ public class Controller : MonoBehaviour {
 	private bool left;
 	private bool right;
 	private bool forward;
+	private bool fire;
 	void Awake()
 	{
 		rigid = GetComponent<Rigidbody2D>();
@@ -17,6 +18,24 @@ public class Controller : MonoBehaviour {
 		right = Input.GetKey(KeyCode.A);
 		left = Input.GetKey(KeyCode.D);
 		forward = Input.GetKey(KeyCode.W);
+		fire = Input.GetKeyDown(KeyCode.Space);
+		// fire function
+		if(fire) Fire();
+	}
+	public GameObject bulletPrefab;
+
+	/// <summary>
+    /// fire button 
+    /// </summary>
+	private void Fire()
+	{
+		GameObject bullet = Instantiate(bulletPrefab);
+		bullet.transform.position = transform.position;
+		bullet.transform.rotation = transform.rotation;
+		
+		// Rigidbody 
+		Rigidbody2D rbdy = bullet.GetComponent<Rigidbody2D>();
+		rbdy.velocity = transform.rotation * new Vector2(0,10.0f);
 	}
 
 	private Rigidbody2D rigid;
@@ -28,10 +47,11 @@ public class Controller : MonoBehaviour {
 		rigid.AddForce( Vector3.down * (rigid.mass * 0.5f));
 
 		transform.rotation = Quaternion.Slerp(transform.rotation, transform.rotation * Quaternion.Euler(0,0,left ? -1.0f*speed : right ? 1.0f*speed : 0.0f),Time.deltaTime);
-		//Camera.current.ScreenToWorldPoint(Screen.width,0);
+
 		Vector2 force = new Vector2(0,forward ? 1.0f : 0.01f);
 		force = force * 10.0f;
 		rigid.AddForce(transform.rotation * force);
+
 		
 	}
 }
